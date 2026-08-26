1a5614b31a80f448694c7b6833e255132d7cae10
RomilRao23 <subyadav07@gmail.com>
2026-08-26 18:22:41 +0000

    functions1

---
+++ 01_Basics/functions.js
@@ -0,0 +1,8 @@
+function addTwo(n1,n2=1){
+    let result=n1+n2;
+    return result;
+    console.log("value of addition is", result);  
+}
+
+const result=addTwo(5)
+console.log("Value of addition is", result);
