26674db77cd9d46ae7f3de235f873dd076140820
RomilRao23 <subyadav07@gmail.com>
2026-08-29 11:06:08 +0000

    Function with object

--- 01_Basics/functions.js
+++ 01_Basics/functions.js
@@ -1,8 +1,22 @@
-function addTwo(n1,n2=1){
-    let result=n1+n2;
-    return result;
-    console.log("value of addition is", result);  
+// function addTwo(n1,n2=1){
+//     let result=n1+n2;
+//     return result;
+//     console.log("value of addition is", result);  
+// }
+
+// const result=addTwo(5)
+// console.log("Value of addition is", result);
+
+
+// Function with object:
+
+const course={
+    username:"Romil",
+    price: 200
 }
 
-const result=addTwo(5)
-console.log("Value of addition is", result);
+function courseInfo(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+courseInfo(course);
